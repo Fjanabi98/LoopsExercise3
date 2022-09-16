2bb703233dbc1ba7f684c3dae1ee68285fc9fc6d
Fahad Janabi <Fahad.janabi98@yahoo.com>
2022-09-16 15:35:24 -0400

    LoopsExercise3

---
+++ LoopsExercise3/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsExercise3
+{
+    internal class Methods
+    {
+        public static void PrintNumbers()
+        {
+           for (int i = 1000; i >=-1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ LoopsExercise3/Program.cs
@@ -0,0 +1,5 @@
+﻿using LoopsExercise3;
+
+
+
+Methods.PrintNumbers();
